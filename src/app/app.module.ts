b0ded3426e9e6a27c6f615de8342a8071d15f435
HP PC <shubhamhalvadia@gmail.com>
2021-06-21 12:49:19 +0530

    Frist commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { BlockViewComponent } from './components/block-view/block-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainViewerComponent,
+    BlockViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/services/blockchain.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Blockchain } from 'vcoin/src/Blockchain'
+import EC from "elliptic";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlockchainService {
+
+  public blockchainInstance = new Blockchain();
+  public walletKeys = [];
+
+  constructor() {
+    this.blockchainInstance.difficulty = 4;
+    this.blockchainInstance.minePendingTransaction('my-wallet-address');
+
+    this.generateWalletKeys();
+  }
+
+  getBlocks(){
+    return this.blockchainInstance.chain;
+  }
+
+  private generateWalletKeys(){
+    const ec = new EC.ec('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj: key,
+      publicKey: key.getPublic('hex'),
+      privateKey: key.getPrivate('hex'),
+    });
+
+  }
+}
